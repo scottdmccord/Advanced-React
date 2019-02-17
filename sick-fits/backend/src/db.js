b269bf3c1e126b503c35eef9acce156da6fff121
Scott McCord <scott.sdm27@gmail.com>
2019-02-17 11:50:22 -0500

    Halfway through GraphQL Yoga server video

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,16 @@
+// this file connects to the remote prisma db and gives us
+// the ability to query it with JS
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    // typeDefs - the generated prisma.graphql file
+    // this is why we pull it down in the post deploy
+    // hook.
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: true,
+});
+
+modules.exports= db;
